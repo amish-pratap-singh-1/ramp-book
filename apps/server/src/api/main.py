8e2b562b978ba8fcb4dc0096bdf620e0fd5b224c
amish-pratap-singh-1 <amishpratapsingh13@gmail.com>
2026-04-11 20:36:10 +0530

    added cors

--- apps/server/src/api/main.py
+++ apps/server/src/api/main.py
@@ -1,12 +1,23 @@
 """Fastapi app"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.auth import router as auth_router
 from src.api.health import router as health_router
 from src.svc.errsvc import AppError, ErrSvc
 
 app = FastAPI(redoc_url=None)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",  # Next.js
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],  # IMPORTANT (includes OPTIONS)
+    allow_headers=["*"],
+)
 
 # Global error handling
 app.add_exception_handler(AppError, ErrSvc.app_error_handler)
